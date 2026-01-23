ade33603e11a2e3619fb3e8451e68e295cd4718c
somandhir <somandhir@gmail.com>
2026-01-23 21:56:49 +0530

    add user account management controllers: change password, fetch current user, update details, and manage avatars

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -29,7 +29,7 @@ const userSchema = new Schema(
       type: String, // cloudinary url
       required: true,
     },
-    converImage: {
+    coverImage: {
       type: String, // cloudinary url
     },
     watchHistory: [
